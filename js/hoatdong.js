1374ee6c70c8d0cbf7ce2b487c6d4863612dea39
unknown <ngocanh6apro@gmail.com>
2020-09-20 22:39:05 +0700

    fewf

--- js/hoatdong.js
+++ js/hoatdong.js
@@ -1,4 +1,24 @@
 $(document).ready(function () {
     TweenMax.staggerFrom($('.bt2121'), 1.5, {y:100, opacity: 0}, 0.4);
     TweenMax.staggerFrom($('.bt21'), 1.5, {y:100, opacity: 0}, 0.4);
+    $('[data-fancybox = "images-album1"]').fancybox({
+        thumbs:{
+          autoStart:true
+        }
+      });
+      $('[data-fancybox = "images-album2"]').fancybox({
+        thumbs:{
+          autoStart:true
+        }
+      });
+      $('[data-fancybox = "images-album3"]').fancybox({
+        thumbs:{
+          autoStart:true
+        }
+      });
+      $('[data-fancybox = "images-album4"]').fancybox({
+        thumbs:{
+          autoStart:true
+        }
+      });
 });
