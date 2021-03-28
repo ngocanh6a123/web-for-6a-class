f1f2c9e9a06bb669930c6403e3e1cbdde7af7adb
unknown <ngocanh6apro@gmail.com>
2021-03-28 18:30:45 +0700

    28/03 thay doi giao dien

--- js/hoatdong.js
+++ js/hoatdong.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    TweenMax.staggerFrom($('.bt2121'), 1.5, {y:100, opacity: 0}, 0.4);
-    TweenMax.staggerFrom($('.bt21'), 1.5, {y:100, opacity: 0}, 0.4);
+    // TweenMax.staggerFrom($('.bt2121'), 1.5, {y:100, opacity: 0}, 0.4);
+    // TweenMax.staggerFrom($('.bt21'), 1.5, {y:100, opacity: 0}, 0.4);
     $('[data-fancybox = "images-album1"]').fancybox({
         thumbs:{
           autoStart:true
--- js/home.js
+++ js/home.js
@@ -1,5 +1,23 @@
 $(document).ready(function () {
-     TweenMax.staggerFrom($(".p_and_img"), 2.5, { ease: Bounce.easeOut, left: -500 });
-     TweenMax.staggerFrom($(".imgsssssssssss"), 2.5, { ease: Bounce.easeOut, right: -500 });
-     TweenMax.staggerFrom($(".chung img"), 2.5 , { ease:  Power2.easeOut, y: 500 }, 0.2);
+     // TweenMax.staggerFrom($(".p_and_img"), 2.5, { ease: Bounce.easeOut, left: -500 });
+     // TweenMax.staggerFrom($(".imgsssssssssss"), 2.5, { ease: Bounce.easeOut, right: -500 });
+     // TweenMax.staggerFrom($(".chung img"), 2.5 , { ease:  Power2.easeOut, y: 500 }, 0.2);
+
+     var swiper = new Swiper('.slide_banner .swiper-container', {
+          scrollbar: {
+            el: '.swiper-scrollbar',
+            hide: true,
+          },
+        });
+
+        var swipers = new Swiper('.fog .swiper-container', {
+          speed: 500,
+          autoplay: {
+               delay: 3000,
+             },
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+          },
+        });
 });
